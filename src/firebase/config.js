c1538ddb2186cc305b6b8f5103ac724958e956e0
Ignacio Trevisan <nachotizii988@gmail.com>
2024-08-03 11:20:26 -0300

    info de config actualizada

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -16,24 +16,24 @@ const {
 } = GetEnvironments();
 
 
-// dev/prod
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCt_Mpe2r8Hm8mLy6bWFFTmiQLTC_twBxk",
-//     authDomain: "react-curso-441bf.firebaseapp.com",
-//     projectId: "react-curso-441bf",
-//     storageBucket: "react-curso-441bf.appspot.com",
-//     messagingSenderId: "31459251569",
-//     appId: "1:31459251569:web:175ca2ac5b83524c6852d0"
-// };
-
+//dev/prod
 const firebaseConfig = {
-    apiKey: VITE_APIKEY,
-    authDomain: VITE_AUTHDOMAIN,
-    projectId: VITE_PROJECTID,
-    storageBucket: VITE_STORAGEBUCKET,
-    messagingSenderId: VITE_MESSAGINGSENDERID,
-    appId: VITE_APPID,
+    apiKey: "AIzaSyCt_Mpe2r8Hm8mLy6bWFFTmiQLTC_twBxk",
+    authDomain: "react-curso-441bf.firebaseapp.com",
+    projectId: "react-curso-441bf",
+    storageBucket: "react-curso-441bf.appspot.com",
+    messagingSenderId: "31459251569",
+    appId: "1:31459251569:web:175ca2ac5b83524c6852d0"
 };
+
+// const firebaseConfig = {
+//     apiKey: VITE_APIKEY,
+//     authDomain: VITE_AUTHDOMAIN,
+//     projectId: VITE_PROJECTID,
+//     storageBucket: VITE_STORAGEBUCKET,
+//     messagingSenderId: VITE_MESSAGINGSENDERID,
+//     appId: VITE_APPID,
+// };
 
 console.log(firebaseConfig)
 
